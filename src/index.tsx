45885a0b22d2274646029d2e36afc76edab93e0d
Sooraj Shukla <sooraj.shukla@brightscout.com>
2022-09-02 12:36:47 +0530

    [MI-2116]: Configuration for making a build to publish on npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,70 @@
-// Used for testing and viewing the components
-import React from 'react';
-import ReactDOM from 'react-dom';
+import AutoSuggest from '@Components/AutoSuggest';
+import MenuButtons from '@Components/Buttons/MenuButtons';
+import BaseCard from '@Components/Card/Base';
+import SubscriptionCard from '@Components/Card/Subscription';
+import Card from '@Components/Card';
+import Checkbox from '@Components/Checkbox';
+import ConfirmationDialog from '@Components/ConfirmationDialog';
+import ConfirmationPanel from '@Components/ConfirmationPanel';
+import Dropdown from '@Components/Dropdown';
+import EmptyState from '@Components/EmptyState';
+import InputField from '@Components/InputField';
+import CircularLoader from '@Components/Loader/Circular';
+import LinearLoader from '@Components/Loader/Linear';
+import SkeletonLoader from '@Components/Loader/Skeleton';
+import CustomModal from '@Components/Modal/customModal';
+import ModalBody from '@Components/Modal/subComponents/ModalBody';
+import ModalFooter from '@Components/Modal/subComponents/ModalFooter';
+import ModalHeader from '@Components/Modal/subComponents/ModalHeader';
+import ModalLoader from '@Components/Modal/subComponents/ModalLoader';
+import ModalSubtitleAndError from '@Components/Modal/subComponents/ModalSubtitleAndError';
+import Modal from '@Components/Modal';
+import Popover from '@Components/Popover';
+import ResultPanel from '@Components/ResultPanel';
+import SvgWrapper from '@Components/SvgWrapper';
+import Tabs from '@Components/Tabs';
+import ToggleSwitch from '@Components/ToggleSwitch';
+import Tooltip from '@Components/Tooltip';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Icons
+import { ServiceNowIcon, BellIcon, UnlinkIcon, LinkIcon, EditIcon, DeleteIcon, CheckIcon, UserIcon } from '@Components/Icons';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root') as HTMLElement
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export {
+    AutoSuggest,
+    MenuButtons,
+    BaseCard,
+    SubscriptionCard,
+    Card,
+    Checkbox,
+    ConfirmationDialog,
+    ConfirmationPanel,
+    Dropdown,
+    EmptyState,
+    // Icons,
+    InputField,
+    CircularLoader,
+    LinearLoader,
+    SkeletonLoader,
+    CustomModal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    ModalLoader,
+    ModalSubtitleAndError,
+    Modal,
+    Popover,
+    ResultPanel,
+    SvgWrapper,
+    Tabs,
+    ToggleSwitch,
+    Tooltip,
+    // Icons
+    ServiceNowIcon,
+    BellIcon,
+    UnlinkIcon,
+    LinkIcon,
+    EditIcon,
+    DeleteIcon,
+    CheckIcon,
+    UserIcon
+}
