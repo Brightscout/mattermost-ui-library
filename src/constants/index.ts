0c7eb2b611e3ef944cff6485f553a83607f21c6c
Sooraj Shukla <sooraj.shukla@brightscout.com>
2022-09-26 20:44:16 +0530

    [MI-2121_4]: review fixes, fix lint issues

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,4 +7,5 @@ export enum ToggleSwitchLabelPositioningValues {
 
 export const TooltipDefaultPlacement = 'top';
 
+// TODO: Remove the constant from here and use from "mattermost-redux" instead
 export const PrivateChannelType = 'P';
