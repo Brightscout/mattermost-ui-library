c8059046c238b6f7e8ebd750f5bba0cf6e6cef56
tanisha-agarwal <tanisha.agarwal@joshtechnologygroup.com>
2022-10-19 13:01:52 +0530

    MI-2263: Spinner Component

---
+++ src/components/Spinner/Spinner.component.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {Icon} from '@Components/Icon';
+
+import {SizeMapType, SpinnerProps} from './Spinner';
+import {SpinnerContainer} from './Spinner.styles';
+
+/**
+ * Spinner Component
+ *
+ * @example Correct usage
+ *
+ * ```ts
+ * <Spinner />
+ * ```
+ *
+ * @example usage with overlay ,size and color
+ *
+ * ```ts
+ * <Spinner overlay size='md' color='green' />
+ * ```
+ */
+export const Spinner = (props: SpinnerProps) => {
+    const {size = 'md', overlay = false, color, className} = props;
+    const sizeMap: SizeMapType = {
+        xs: 10,
+        sm: 12,
+        md: 16,
+        lg: 20,
+        xl: 28,
+        '2xl': 32,
+    };
+
+    return (
+        <SpinnerContainer
+            overlay={overlay}
+            color={color}
+            className={className}
+        >
+            <Icon
+                name='Spinner'
+                iconColor='transparent'
+                size={sizeMap[size]}
+            />
+        </SpinnerContainer>
+    );
+};
