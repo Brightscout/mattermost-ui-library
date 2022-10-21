25f520df1b1f3427ae09492348da63549a9019a8
tanisha-agarwal <tanisha.agarwal@joshtechnologygroup.com>
2022-10-21 14:33:05 +0530

    MI-2276: Tooltip Component

---
+++ src/components/Tooltip/Tooltip.component.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {OverlayTrigger} from 'react-bootstrap';
+
+import {Icon} from '@Components/Icon';
+
+import {TooltipProps} from './Tooltip';
+import {Hint, Text, StyledTooltip} from './Tooltip.styles';
+
+/**
+ * Tooltip Component
+ *
+ * @example Correct usage
+ *
+ * ```ts
+ * <Tooltip text='text'>
+ *  <SomeComponent/>
+ * </Tooltip>
+ *
+ * @example usage with icon and hint text
+ *
+ * ```ts
+ * <Tooltip text='text' hint='hint text' iconName='Edit'>
+ *  <SomeComponent/>
+ * </Tooltip>
+ *
+ */
+export const Tooltip = (props: TooltipProps) => {
+    const {children, placement, iconName, text, hint} = props;
+
+    const renderedToolkit = () => (
+        <StyledTooltip id={'tooltip'}>
+            <Text>
+                {iconName &&
+                <Icon
+                    name={iconName}
+                    size={12}
+                />}
+                {text}
+            </Text>
+            {hint && <Hint>{hint}</Hint>}
+        </StyledTooltip>
+    );
+
+    return (
+        <OverlayTrigger
+            key={placement}
+            placement={placement}
+            flip={true}
+            delay={400}
+            overlay={renderedToolkit()}
+        >
+            {children}
+        </OverlayTrigger>
+    );
+};
