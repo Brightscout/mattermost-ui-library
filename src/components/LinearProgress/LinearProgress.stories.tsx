56a70a90c4e40db085dea154168cfec4965d9f3e
tanisha-agarwal <tanisha.agarwal@joshtechnologygroup.com>
2022-11-03 17:45:19 +0530

    MI_2268: Refactoring indendation in storybook

--- src/components/LinearProgress/LinearProgress.stories.tsx
+++ src/components/LinearProgress/LinearProgress.stories.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import {Story, Meta} from '@storybook/react';
 
-import { LinearProgress } from './LinearProgress.component';
-import { LinearProgressProps } from './LinearProgress';
+import {LinearProgress} from './LinearProgress.component';
+import {LinearProgressProps} from './LinearProgress';
 
 export default {
-	title: 'Component/LinearProgress',
-	component: LinearProgress,
-	argTypes: {
-		value: {
-			control: { type: 'range', min: 0, max: 100 },
-		},
-	},
+    title: 'Component/LinearProgress',
+    component: LinearProgress,
+    argTypes: {
+        value: {
+            control: {type: 'range', min: 0, max: 100},
+        },
+    },
 } as Meta;
 
-const LinearProgressTemplate: Story<LinearProgressProps> = (args) => (
-	<LinearProgress {...args} />
-);
+const LinearProgressTemplate: Story<LinearProgressProps> = (args) => <LinearProgress {...args}/>;
 
 // Default
 export const Default = LinearProgressTemplate.bind({});
@@ -24,12 +22,12 @@ export const Default = LinearProgressTemplate.bind({});
 // Determinate linear progress
 export const Determinate = LinearProgressTemplate.bind({});
 Determinate.args = {
-	determinate: true,
-	value: 20,
+    determinate: true,
+    value: 20,
 };
 
 // Linear progress with color
 export const LinearProgressWithColor = LinearProgressTemplate.bind({});
 LinearProgressWithColor.args = {
-	color: '#e76f51',
+    color: '#e76f51',
 };
