ec16ca9c5727550812c99ceb26d480c7cc778eb7
Tanisha Agarwal <tanisha.agarwal@joshtechnologygroup.com>
2022-11-07 20:02:00 +0530

    MI-2333: Fix ProgressBar issue on deploying storybook on chromatic

--- src/components/Loader/Linear/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LinearLoader = () : JSX.Element => (
-    <div className='linear-loader border-radius-4'>
-        <div className='linear-loader__bar border-radius-4'/>
-    </div>
-);
-
-export default LinearLoader;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 // New Components
-import {Icon} from '@Components/Icon';
 import {Button} from '@Components/Button';
 import {Checkbox} from '@Components/CheckBox';
+import {Icon} from '@Components/Icon';
+import {Input} from '@Components/Input';
+import {LinearProgress} from '@Components/LinearProgress';
+import {Spinner} from '@Components/Spinner';
 import {Switch} from '@Components/Switch';
 
 export {
     Button,
     Checkbox,
     Icon,
+    Input,
+    LinearProgress,
+    Spinner,
     Switch,
 };
