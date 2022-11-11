65413a3c7cb5172b49d35494fa9de392713f46df
tanisha-agarwal <102018792+tanisha-agarwal@users.noreply.github.com>
2022-11-11 19:49:20 +0530

    MI-2277: Replaced esbuild with webpack (#44)
    MI-2277: Replaced esbuild with webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'umd',
+        library: 'mattermost-ui-library'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: ['babel-loader', 'ts-loader'],
+            exclude: /node_modules/,
+          },
+          {
+            test: /\.(sc|c)ss$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'postcss-loader',
+              'sass-loader',
+            ],
+          },
+          {
+            test: /\.(svg)$/,
+            use: ['@svgr/webpack', 'url-loader'],
+          },
+        ]
+    },
+    resolve: {
+      plugins: [new TsconfigPathsPlugin()],
+      extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    devtool: 'source-map',
+    externals: {
+        react: 'react',
+        'react/jsx-runtime': 'react/jsx-runtime',
+        'react-dom': 'react-dom',
+        'styled-components': 'styled-components',
+    }
+}
